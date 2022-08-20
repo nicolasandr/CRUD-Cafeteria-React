1056367020604b4bc9119de3eae6f6cbac7b921e
nicolas andrade <nicolasandrade328@gmail.com>
2022-08-19 21:45:13 -0300

    se agrego el maquetado de la pagina para administrar los productos

--- src/components/views/AdministrarProductos.js
+++ src/components/views/AdministrarProductos.js
@@ -1,11 +1,32 @@
-import React from 'react';
-
+import React from "react";
+import { Table, Button } from "react-bootstrap";
+import ItemProducto from "./ItemProducto";
 const AdministrarProductos = () => {
-    return (
-        <div>
-            <h1>administrar productos</h1>
-        </div>
-    );
+  return (
+    <section className="container">
+      <div className="d-flex justify-content-between align-items-center mt-5">
+        <h1 className="display-4 ">Poductos disponibles</h1>
+        <Button variant="primary">Agregar</Button>
+      </div>
+
+      <hr />
+      <Table responsive striped bordered hover>
+        <thead>
+          <tr>
+            <th>Cod</th>
+            <th>Producto</th>
+            <th>Precio</th>
+            <th>URL de Imagen</th>
+            <th>Categoria</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* aqui tengo que hacer un map */}
+          <ItemProducto></ItemProducto>
+        </tbody>
+      </Table>
+    </section>
+  );
 };
 
 export default AdministrarProductos;
